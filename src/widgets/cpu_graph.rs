369489bcf7d945533d8aa6c36d4b0d4ea2adeae8
acxz <17132214+acxz@users.noreply.github.com>
2024-07-28 20:03:43 -0500

    wip

--- src/widgets/cpu_graph.rs
+++ src/widgets/cpu_graph.rs
@@ -7,8 +7,8 @@ use crate::{
     app::AppConfigFields,
     canvas::{
         components::data_table::{
-            Column, ColumnHeader, DataTable, DataTableColumn, DataTableProps, DataTableStyling,
-            DataToCell,
+            ColumnHeader, DataTableColumn, DataTableProps, DataTableStyling, DataToCell,
+            SortColumn, SortDataTable, SortDataTableProps, SortOrder, SortsRow
         },
         styling::CanvasStyling,
         Painter,
@@ -55,6 +55,8 @@ impl ColumnHeader for CpuWidgetColumn {
     }
 }
 
+// TODO: Do I need this?
+// #[derive(Clone)]
 pub enum CpuWidgetTableData {
     All,
     Entry {
@@ -156,12 +158,120 @@ impl DataToCell<CpuWidgetColumn> for CpuWidgetTableData {
     }
 }
 
+impl SortsRow for CpuWidgetColumn {
+    type DataType = CpuWidgetTableData;
+
+    fn sort_data(&self, data: &mut [Self::DataType], descending: bool) {
+        // let mut table_data = data.iter()
+        //                          .map(CpuWidgetTableData::from_cpu_widget_data)
+        //                          .collect()
+        match self {
+            // TODO: don't sort ALL and AVG
+            CpuWidgetColumn::CPU => {
+                data.sort_by(|a, b| {
+                    let mut order = match (a, b) {
+                        (CpuWidgetTableData::All, _) => std::cmp::Ordering::Greater,
+                        (_, CpuWidgetTableData::All) => std::cmp::Ordering::Less,
+                        (CpuWidgetTableData::Entry {
+                            data_type: a_data_type, ..},
+                        CpuWidgetTableData::Entry {
+                            data_type: b_data_type, ..}
+                        ) => {
+                            match (a_data_type, b_data_type) {
+                                (CpuDataType::Avg, _) => std::cmp::Ordering::Greater,
+                                (_, CpuDataType::Avg) => std::cmp::Ordering::Less,
+                                // TODO: does this get the name field?
+                                (CpuDataType::Cpu(a_cpu), CpuDataType::Cpu(b_cpu)) => a_cpu.cmp(b_cpu),
+                            }
+                        },
+                    };
+                    // TODO: factor in descending bool
+                    if !descending {
+                        // Flip order
+                        order = match order {
+                            std::cmp::Ordering::Less => std::cmp::Ordering::Greater,
+                            std::cmp::Ordering::Greater => std::cmp::Ordering::Less,
+                            _ => order,
+                        }
+                    }
+                    return order;
+                });
+            }
+            CpuWidgetColumn::Use => {
+                data.sort_by(|a, b| {
+                    let mut order = match (a, b) {
+                        (CpuWidgetTableData::All, _) => std::cmp::Ordering::Greater,
+                        (_, CpuWidgetTableData::All) => std::cmp::Ordering::Less,
+                        (CpuWidgetTableData::Entry {
+                            data_type: a_data_type,
+                            last_entry: a_last_entry
+                        },
+                        CpuWidgetTableData::Entry {
+                            data_type: b_data_type,
+                            last_entry: b_last_entry
+                        }) => {
+                            match (a_data_type, b_data_type) {
+                                (CpuDataType::Avg, _) => std::cmp::Ordering::Greater,
+                                (_, CpuDataType::Avg) => std::cmp::Ordering::Less,
+                                // TODO: does this get the usage field?
+                                (CpuDataType::Cpu(_), CpuDataType::Cpu(_)) => a_last_entry.partial_cmp(b_last_entry).unwrap_or(std::cmp::Ordering::Equal),
+                            }
+                        },
+                    };
+                    // TODO: factor in descending bool
+                    if !descending {
+                        // Flip order
+                        order = match order {
+                            std::cmp::Ordering::Less => std::cmp::Ordering::Greater,
+                            std::cmp::Ordering::Greater => std::cmp::Ordering::Less,
+                            _ => order,
+                        }
+                    }
+                    return order;
+
+                    // let order = std::cmp::Ordering::Equal;
+                    // if a == CpuWidgetData::All {
+                    //     std::cmp::Ordering::Greater;
+                    // } else if a == CpuWidgetData::Entry { // a == Avg
+                    //     if b == CpuWidgetData::All {
+                    //         std::cmp::Ordering::Less;
+                    //     } else {
+                    //         std::cmp::Ordering::Greater;
+                    //     }
+                    // } else {
+                    //     if b == CpuWidgetData::All || b == CpuWidgetData::Entry { // b == Avg
+                    //         std::cmp::Ordering::Less;
+                    //     }
+                    //     else {
+                    //         // TODO: does this get the use field?
+                    //         // TODO: run num compare on a and b cpuusage
+                    //     }
+                    // }
+                });
+            }
+        }
+    }
+}
+
+// TODO: required for sorting cpuwidgetdatas in the SortsRow impl
+// impl PartialOrd for CpuWidgetData {
+// 
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         match (self, other) {
+//             (CpuWidgetData::All, CpuWidget::Entry(_, _, _)) => Some(Greater),
+//             (CpuWidgetData::Entry(_, _, _), CpuWidgetData::All) => Some(Less),
+//             (CpuWidgetData::Entry(data_type_a, data_a, last_entry_a), CpuWidgetData::Entry(data_type_b, data_b, last_entry_b)) => //TODO: Match this on cpu or use,
+//             _ => unreachable!(),
+//         }
+//     }
+// }
+
 pub struct CpuWidgetState {
     pub current_display_time: u64,
     pub is_legend_hidden: bool,
     pub show_avg: bool,
     pub autohide_timer: Option<Instant>,
-    pub table: DataTable<CpuWidgetTableData, CpuWidgetColumn>,
+    pub table: SortDataTable<CpuWidgetTableData, CpuWidgetColumn>,
     pub styling: CpuWidgetStyling,
 }
 
@@ -170,22 +280,27 @@ impl CpuWidgetState {
         config: &AppConfigFields, default_selection: CpuDefault, current_display_time: u64,
         autohide_timer: Option<Instant>, colours: &CanvasStyling,
     ) -> Self {
-        const COLUMNS: [Column<CpuWidgetColumn>; 2] = [
-            Column::soft(CpuWidgetColumn::CPU, Some(0.5)),
-            Column::soft(CpuWidgetColumn::Use, Some(0.5)),
+        let columns: [SortColumn<CpuWidgetColumn>; 2] = [
+            SortColumn::soft(CpuWidgetColumn::CPU, None).default_descending(),
+            SortColumn::soft(CpuWidgetColumn::Use, None),
         ];
 
-        let props = DataTableProps {
-            title: None,
-            table_gap: config.table_gap,
-            left_to_right: false,
-            is_basic: false,
-            show_table_scroll_position: false, // TODO: Should this be possible?
-            show_current_entry_when_unfocused: true,
+        let props = SortDataTableProps {
+            inner: DataTableProps {
+                title: None,
+                table_gap: config.table_gap,
+                left_to_right: false,
+                is_basic: false,
+                show_table_scroll_position: config.show_table_scroll_position,
+                show_current_entry_when_unfocused: true,
+            },
+            sort_index: 0,
+            order: SortOrder::Ascending
         };
 
         let styling = DataTableStyling::from_colours(colours);
-        let mut table = DataTable::new(COLUMNS, props, styling);
+
+        let mut table = SortDataTable::new_sortable(columns, props, styling);
         match default_selection {
             CpuDefault::All => {}
             CpuDefault::Average if !config.show_average_cpu => {}
@@ -204,11 +319,22 @@ impl CpuWidgetState {
         }
     }
 
-    pub fn update_table(&mut self, data: &[CpuWidgetData]) {
-        self.table.set_data(
-            data.iter()
-                .map(CpuWidgetTableData::from_cpu_widget_data)
-                .collect(),
-        );
+    pub fn update_table(&mut self, data: const &[CpuWidgetData]) {
+        // TODO: This is the funky stuff
+        let mut table_data = [CpuWidgetTableData::All; data.len()];
+        for (idx, cpu_widget_data) in data.iter().enumerate() {
+            table_data[idx] = CpuWidgetTableData::from_cpu_widget_data(cpu_widget_data);
+        }
+
+        // let mut data = data.to_vec();
+        if let Some(column) = self.table.columns.get(self.table.sort_index()) {
+            column.sort_by(&mut table_data, self.table.order());
+        }
+        self.table.set_data(table_data);
+        // self.table.set_data(
+        //     data.iter()
+        //         .map(CpuWidgetTableData::from_cpu_widget_data)
+        //         .collect(),
+        // );
     }
 }
